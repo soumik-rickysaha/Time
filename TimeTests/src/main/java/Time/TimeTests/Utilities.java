518e129cb8c76327e7efe4822ccf7c7ec442b3be
soumik-rickysaha <soumik.rickysaha@gmail.com>
2018-04-26 01:30:29 +0530

    Commit : 1

---
+++ TimeTests/src/main/java/Time/TimeTests/Utilities.java
@@ -0,0 +1,48 @@
+package Time.TimeTests;
+
+import java.io.File;
+
+/**
+ * Hello world!
+ *
+ */
+public class Utilities {
+	String TimeFolderPath = "D:\\Time All Question Papers";
+	String Aimcat = TimeFolderPath + "\\AIMCATS";
+	String Samcats = TimeFolderPath + "\\SAMCATS";
+	String Omets = TimeFolderPath + "\\OMETS";
+	String AimOmets = TimeFolderPath + "\\AIMOMETS";
+	File Fileobj;
+
+	public String createFolder(String strTestName, String TargetLocation) {
+		String Fullpath = null;
+
+		switch (TargetLocation.toUpperCase()) {
+
+		case "AIMCAT":
+			Fullpath = TimeFolderPath + Aimcat;
+			break;
+		case "SAMCATS":
+			Fullpath = TimeFolderPath + Samcats;
+			break;
+		case "OMETS":
+			Fullpath = TimeFolderPath + Omets;
+			break;
+		case "AIMOMETS":
+			Fullpath = TimeFolderPath + AimOmets;
+			break;
+		default:
+			System.out.println("issue in path select case");
+			break;
+		}
+
+		// create Directory with testname
+		Fileobj = new File(Fullpath + "\\" + strTestName);
+		if (!Fileobj.exists()) {
+			Fileobj.mkdir();
+			
+		}
+		return Fullpath + "\\" + strTestName;
+
+	}
+}
